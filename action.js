75a6b5d2bb2a6d7a6828f4bf067937c4eebf2617
Darya Radzevich <ganocij1@gmail.com>
2021-01-15 12:46:26 +0300

    init: start create app

---
+++ action.js
@@ -0,0 +1,133 @@
+const body = document.querySelector("#body");
+const refreshBg = document.querySelector(".refresh-background");
+const languageEn = document.querySelector(".language-button-en");
+const languageRu = document.querySelector(".language-button-ru");
+const temperatureF = document.querySelector(".temperature-button-farenheit");
+const temperatureC = document.querySelector(".temperature-button-celsius");
+const searchInput = document.querySelector(".search-input");
+const searchButton = document.querySelector(".search-button");
+const cityAndCountry = document.querySelector(".weather-city");
+const dateTimeWeather = document.querySelector(".weather-date-and-time");
+const temperatureNow = document.querySelector(".weather-temperature");
+const weatherImg = document.querySelector(".weather-image");
+const weatherDay = document.querySelector(".weather-day");
+const forecastTemperature = document.querySelector(
+  ".weather-forecast-temperature"
+);
+const forecastImg = document.querySelector(".weather-forecast-image");
+const map = document.querySelector("#map");
+const latitude = document.querySelector(".latitude");
+const longitude = document.querySelector(".longitude");
+let posLatitude;
+let posLongitude;
+let address;
+let city;
+
+//--------refresh background----------------
+const getLinkToImage = async () => {
+  const url =
+    "https://api.unsplash.com/photos/random?&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17";
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.urls.regular;
+};
+
+async function getBackground() {
+  try {
+    const backgroundLink = await getLinkToImage();
+    body.style.backgroundImage = `url(${backgroundLink})`;
+    body.style.transition = "1s";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+refreshBg.addEventListener("click", getBackground);
+
+//------------latitude------longitude----------------------------------
+
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      longitude.textContent = `Longitude: ${position.coords.longitude.toFixed(
+        2
+      )}`;
+      latitude.textContent = `Latitude: ${position.coords.latitude.toFixed(2)}`;
+    });
+  }
+});
+
+//-----date-time----------------------------------
+function showTime() {
+  let today = new Date();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+  let d = today.getDay();
+  let m = today.getMonth();
+  let date = today.getDate();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  dateTimeWeather.textContent = `${days[d]} ${date} ${
+    months[m]
+  }   ${hour}:${addZero(min)}:${addZero(sec)}`;
+  setTimeout(showTime, 1000);
+}
+
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+showTime();
+
+//----------------map---------------
+function createMap() {
+  navigator.geolocation.getCurrentPosition(showMap);
+  function showMap(position) {
+    posLatitude = position.coords.latitude.toFixed(2);
+    posLongitude = position.coords.longitude.toFixed(2);
+    getMap(posLatitude, posLongitude);
+  }
+}
+
+createMap();
+
+function getMap(posLatitude, posLongitude) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZ2Fub2NpaiIsImEiOiJja2pzZXQzdTEwMnpzMnNwOHR2M2drM2YzIn0.dXphpFRS-DDUvodGD7CR8g";
+  let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+  });
+}
+
+//-----search------
+
+
+
+ 
+//-------weather now-------
+
+
+function pressEnter(e) {
+    if (e.which == 13) {
+     
+    }
+  }
+//   searchButton.addEventListener('click', );
+  window.addEventListener('keypress', pressEnter);
+
+const urlWeather3days = "https://api.weatherbit.io/v2.0/forecast/daily?city=Moscow&country=RU&days=3&units=S&lang=be&key=90b5e2275b7d43458feb47f127520564";
